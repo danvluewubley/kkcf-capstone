62b426ef690a21030aa44de03322befc2eb415aa
Daniel Wu <danielw72908@gmail.com>
2024-08-04 17:00:32 -0400

    Reduced chunks

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
 import Nav from "./components/Nav";
 import "./styles/index.css";
+
+// Dynamically import the Home component
+const Home = lazy(() => import("./components/Home"));
+const Projects = lazy(() => import("./components/Projects"));
+const Skills = lazy(() => import("./components/Skills"));
+const Contact = lazy(() => import("./components/Contact"));
 
 function App() {
   return (
     <Router>
       <Nav />
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
       </main>
     </Router>
   );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), visualizer({ open: true })],
+  build: {
+    rollupOptions: {
+      output: {
+        // Manual chunking to split dependencies into separate chunks
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600, // Adjusting the warning limit if needed
+  },
+});
